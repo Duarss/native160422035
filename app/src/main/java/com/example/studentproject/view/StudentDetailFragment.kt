6237e60470b7a4ff9c3ef4a99512e7f69187423d
Duarss <s160422035@student.ubaya.ac.id>
2025-10-06 10:49:31 +0700

    feat: Show toast on data load
    A toast message is now displayed in the Student Detail screen when the student data has been successfully loaded.

--- app/src/main/java/com/example/studentproject/view/StudentDetailFragment.kt
+++ app/src/main/java/com/example/studentproject/view/StudentDetailFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.studentproject.R
@@ -27,6 +28,9 @@ class StudentDetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // baca id student lalu panggil fetch viewmodel
+        // untuk load data tersebut
         val id = StudentDetailFragmentArgs.fromBundle(requireArguments()).id
         // init the vm
         viewModel = ViewModelProvider(this)[DetailViewModel::class.java]
@@ -40,6 +44,7 @@ class StudentDetailFragment : Fragment() {
     fun observeViewModel() {
         viewModel.studentLD.observe(viewLifecycleOwner, Observer {
             student = it
+            Toast.makeText(context, "Data loaded", Toast.LENGTH_SHORT).show()
 
             // update UI
             binding.txtID.setText(student.id)
